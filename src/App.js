989c58daf30fd420e64019e5eab236737ffc008d
Farah Student <farah.gus@hotmail.com>
2019-03-20 17:52:49 -0400

    accordion drill

--- src/App.js
+++ src/App.js
@@ -1,22 +1,28 @@
 import React, { Component } from 'react';
-import Bomb from './state-drill/Bomb';
+import Accordion from './state-drill/Accordion';
 import './App.css';
 
-const tabsProp = [
-  { name: 'First tab',
-    content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam exercitationem quos consectetur expedita consequatur. Fugit, sapiente aspernatur corporis velit, dolor eum reprehenderit provident ipsam, maiores incidunt repellat! Facilis, neque doloremque.' },
-  { name: 'Second tab',
-    content: 'Laboriosam exercitationem quos consectetur expedita consequatur. Fugit, sapiente aspernatur corporis velit, dolor eum reprehenderit provident ipsam, maiores incidunt repellat! Facilis, neque doloremque. Lorem ipsum dolor sit amet consectetur adipisicing elit.' },
-  { name: 'Third tab',
-    content: 'Fugit, sapiente aspernatur corporis velit, dolor eum reprehenderit provident ipsam, maiores incidunt repellat! Facilis, neque doloremque. Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam exercitationem quos consectetur expedita consequatur.' },
-];
+const sections = [
+  {
+    title: 'Section 1',
+    content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit.',
+  },
+  {
+    title: 'Section 2',
+    content: 'Cupiditate tenetur aliquam necessitatibus id distinctio quas nihil ipsam nisi modi!',
+  },
+  {
+    title: 'Section 3',
+    content: 'Animi amet cumque sint cupiditate officia ab voluptatibus libero optio et?',
+  },
+]
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Bomb
-
+        <Accordion
+          accordionSections={sections}
         />
       </div>
     );
--- src/index.js
+++ src/index.js
@@ -1,8 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import RouletteGun from './state-drill/RouletteGun';
 
 import './index.css';
 
-ReactDOM.render(<RouletteGun />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
